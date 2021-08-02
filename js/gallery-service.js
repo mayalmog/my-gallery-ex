280e2cb2af004baf628df6793748b7c982a58863
May Almog <mayalmog@gmail.com>
2021-08-02 21:26:17 +0300

    contact-me form

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -16,8 +16,8 @@ function _createProj(id, name, title, desc, url, publishedAt, labels) {
 }
 
 function createProjs() {
-    gProjs.push(_createProj('minesweeper', 'MineSweeper', 'The popular game with extra features', 'The popular interactive game of mine finding, built using html, css and vanilla JS. Utilizing localStorage.', 'minesweeper.PNG', 'July 2021', ['Matrixes', 'MVC']));
-    gProjs.push(_createProj('bookshop', 'Bookshop', 'Bookshop management site', 'A website that enables any bookshop owner to manage and display inventory, and receive costumer reviews', 'bookshop.PNG', 'July 2021', ['Tables', 'Pagination']));
+    gProjs.push(_createProj('minesweeper', 'MineSweeper', 'The popular game with extra features', 'The popular interactive game of mine finding, built using html, css and vanilla JS.', 'https://mayalmog.github.io/MineSweeper/', 'July 2021', ['Matrixes', 'MVC']));
+    gProjs.push(_createProj('bookshop', 'Bookshop', 'Bookshop management site', 'A website that enables any bookshop owner to manage and display inventory, and receive costumer reviews.', 'https://mayalmog.github.io/Bookshop-demo/', 'July 2021', ['localStorage', 'Pagination']));
 }
 
 function getProjs() {
--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 $(document).ready(onInit);
 
+$('.contact-me-submit-btn').click(onContactMeSubmit);
+
 
 function onInit() {
     createProjs();
@@ -21,7 +23,7 @@ function renderProjs() {
                     <i class="fa fa-plus fa-3x"></i>
                 </div>
             </div>
-            <img class="img-fluid" src="img/portfolio/${proj.url}" alt="">
+            <img class="img-fluid" src="img/portfolio/${proj.id}.png" alt="">
           </a>
             <div class="portfolio-caption">
                 <h4>${proj.name}</h4>
@@ -54,12 +56,13 @@ function renderModals() {
                                     <!-- Project Details Go Here -->
                                     <h2>${proj.name}</h2>
                                     <p class="item-intro text-muted">${proj.title}</p>
-                                    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.url}" alt="">
+                                    <img class="img-fluid d-block mx-auto" src="img/portfolio/${proj.id}.png" alt="">
                                         <p>${proj.desc}</p>
                                         <ul class="list-inline">
                                             <li>Date: ${proj.publishedAt}</li>
                                             <li>Client: Coding Academy</li>
                                             <li>Category: ${proj.labels}</li>
+                                            <li><a href="${proj.url}" target="_label">Open project</a></li>
                                         </ul>
                                         <button class="btn btn-primary" data-dismiss="modal" type="button">
                                             <i class="fa fa-times"></i>
@@ -76,3 +79,18 @@ function renderModals() {
 
     $('.modals-container').html(strHTML);
 }
+
+function onContactMeSubmit() {
+    var userEmail = $('#email').val();
+    var formSubject = $('#subject').val();
+    var formMessage = $('#Message').val();
+
+    console.log(formMessage);
+
+    var contactWindow = `https://mail.google.com/mail/u/0/?fs=1&to=mayalmog@gmail.com&su=${formSubject}&body=${formMessage}&bcc=${userEmail}&tf=cm`;
+
+    window.open(
+        contactWindow, "_blank");
+
+}
+
